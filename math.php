71d871b26277656aa1fab51d5f6e8cba08cc9dd3
Jester <parinojester@gmail.com>
2025-10-01 20:24:47 +0800

    second commit

--- math.php
+++ math.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Task 2: Simple Math</title>
 </head>
 <body>
-    
+    <?php
+    $a=10;
+    $b= 165;
+
+    $sum=$a+$b;
+    $difference=$a-$b;
+    $product=$a*$b;
+    $qoutient=$a/$b;
+
+    echo "Value of A: ".$a ;
+    echo "Value of B: ".$b ;
+    echo "The sum is: ".$sum;
+    echo "The difference is: ".$difference;
+    echo "The product is: ".$product;
+    echo "The qoutient is: ".$qoutient;
+
+    ?>
+
 </body>
 </html>
