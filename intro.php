d564f28ea4f9a2099057fae17e0b3bfcca061e0b
Jester <parinojester@gmail.com>
2025-10-01 20:03:57 +0800

    Initial commit

---
+++ intro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduce Yourself</title>
+</head>
+<body>
+    <h1>Task 1 : Introduce Yourself </h1>
+  <?php 
+  $name="Jester Pariño";
+  $age="20";
+  $color="Light Blue";
+
+  echo "My name is" . $name . "and I'm " . $age . "";
+  
+  ?>
+</body>
+</html>
