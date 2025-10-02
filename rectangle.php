b35b552140e28b43bbbdad19e9f3921cece53215
Jester <parinojester@gmail.com>
2025-10-02 21:21:41 +0800

    commit

--- rectangle.php
+++ rectangle.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Area and Perimeter of a Rectangle</title>
 </head>
 <body>
-    
+    <?php 
+     $length = 0;
+     $width = 0;
+    ?>
 </body>
 </html>
